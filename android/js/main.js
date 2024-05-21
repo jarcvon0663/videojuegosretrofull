3ac60a9d809ea31caecde312ebf44a2916b3ae89
jarcvon0663 <55066370+jarcvon0663@users.noreply.github.com>
2024-05-20 21:19:17 -0500

    Optimización del sitio web

--- android/js/main.js
+++ android/js/main.js
@@ -6,27 +6,21 @@ fetch("./js/productos.json")
         productos = data;
         cargarProductos(productos);
     })
-
+    .catch(error => console.error('Error al cargar los productos:', error));
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
-let botonesAgregar = document.querySelectorAll(".producto-agregar");
+let botonesAgregar;
 const numerito = document.querySelector("#numerito");
-
 
 botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
     aside.classList.remove("aside-visible");
-}))
-
+}));
 
 function cargarProductos(productosElegidos) {
-
-    const contenedorProductos = document.getElementById("contenedor-productos");
     contenedorProductos.innerHTML = "";
-
     productosElegidos.forEach(producto => {
-
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
@@ -34,139 +28,143 @@ function cargarProductos(productosElegidos) {
             <div class="producto-detalles">
                 <h3 class="producto-titulo">${producto.titulo}</h3>
                 <p class="producto-precio">$${producto.precio}</p>
-                <a class="producto-trailer" href="${producto.trailer}" target="_blank"><img src="./img/trailer.png" alt="Trailer"></a>
+                <a class="producto-trailer" href="${producto.trailer}" target="_blank">
+                    <img src="./img/trailer.png" alt="Trailer">
+                </a>
                 <p class="producto-advertencia">${producto.advertencia}</p>
                 <button class="producto-agregar" id="${producto.id}">Agregar</button>
             </div>
         `;
-
         contenedorProductos.append(div);
-    })
+    });
 
     actualizarBotonesAgregar();
 
-    // Actualiza los botones para abrir el pop-up
     const trailerLinks = document.querySelectorAll(".producto-trailer");
     trailerLinks.forEach(link => {
         link.addEventListener("click", function(event) {
             event.preventDefault();
-            const videoUrl = this.href;
-            const videoId = getYoutubeVideoId(videoUrl);
-            const embedUrl = `https://www.youtube.com/embed/${videoId}`;
-            const popup = document.getElementById("popup-trailer");
-            const popupVideo = document.getElementById("popup-video");
-            popupVideo.src = embedUrl;
-            popup.style.display = "block";
+            mostrarTrailer(this.href);
         });
     });
 
-    // Cierra el pop-up
     const popupClose = document.querySelector(".popup-close");
-    popupClose.addEventListener("click", function() {
-        const popup = document.getElementById("popup-trailer");
-        const popupVideo = document.getElementById("popup-video");
-        popup.style.display = "none";
-        popupVideo.src = "";
-    });
+    popupClose.addEventListener("click", cerrarTrailer);
+}
+
+function mostrarTrailer(url) {
+    const videoId = getYoutubeVideoId(url);
+    const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    const popup = document.getElementById("popup-trailer");
+    const popupVideo = document.getElementById("popup-video");
+    popupVideo.src = embedUrl;
+    popup.style.display = "block";
+}
+
+function cerrarTrailer() {
+    const popup = document.getElementById("popup-trailer");
+    const popupVideo = document.getElementById("popup-video");
+    popup.style.display = "none";
+    popupVideo.src = "";
 }
 
 function getYoutubeVideoId(url) {
     const urlObj = new URL(url);
-    if (urlObj.hostname === 'youtu.be') {
-        return urlObj.pathname.slice(1);
-    } else {
-        return urlObj.searchParams.get('v');
-    }
+    return urlObj.hostname === 'youtu.be' ? urlObj.pathname.slice(1) : urlObj.searchParams.get('v');
 }
 
 window.addEventListener("click", function(event) {
     const popup = document.getElementById("popup-trailer");
     if (event.target == popup) {
-        popup.style.display = "none";
-        const popupVideo = document.getElementById("popup-video");
-        popupVideo.src = "";
+        cerrarTrailer();
     }
 });
 
-
 botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) => {
-
         botonesCategorias.forEach(boton => boton.classList.remove("active"));
         e.currentTarget.classList.add("active");
 
-        if (e.currentTarget.id != "todos") {
-            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+        const categoriaId = e.currentTarget.id;
+        if (categoriaId != "todos") {
+            const productosFiltrados = productos.filter(producto => producto.categoria.id === categoriaId);
+            const productoCategoria = productosFiltrados[0];
             tituloPrincipal.innerText = productoCategoria.categoria.nombre;
-            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
-            cargarProductos(productosBoton);
+            cargarProductos(productosFiltrados);
         } else {
             tituloPrincipal.innerText = "Todos los productos";
             cargarProductos(productos);
         }
-
-    })
+    });
 });
 
 function actualizarBotonesAgregar() {
     botonesAgregar = document.querySelectorAll(".producto-agregar");
-
-    botonesAgregar.forEach(boton => {
-        boton.addEventListener("click", agregarAlCarrito);
-    });
+    botonesAgregar.forEach(boton => boton.addEventListener("click", agregarAlCarrito));
 }
 
-let productosEnCarrito;
-
-let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
-
-if (productosEnCarritoLS) {
-    productosEnCarrito = JSON.parse(productosEnCarritoLS);
-    actualizarNumerito();
-} else {
-    productosEnCarrito = [];
-}
+let productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito")) || [];
+actualizarNumerito();
 
 function agregarAlCarrito(e) {
-
-    Toastify({
-        text: "Producto agregado",
-        duration: 3000,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-          background: "linear-gradient(to right, #4b33a8, #785ce9)",
-          borderRadius: "2rem",
-          textTransform: "uppercase",
-          fontSize: ".75rem"
-        },
-        offset: {
-            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
-          },
-        onClick: function(){} // Callback after click
-      }).showToast();
-
     const idBoton = e.currentTarget.id;
     const productoAgregado = productos.find(producto => producto.id === idBoton);
 
-    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
-        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
-        productosEnCarrito[index].cantidad++;
+    // Verificar si el producto ya está en el carrito
+    const productoExistente = productosEnCarrito.find(producto => producto.id === idBoton);
+    if (productoExistente) {
+        // Si el producto ya está en el carrito, mostrar un mensaje
+        Toastify({
+            text: "Este producto ya está en el carrito",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(to right, #ff6347, #ffc0cb)",
+                borderRadius: "2rem",
+                textTransform: "uppercase",
+                fontSize: ".75rem"
+            },
+            offset: {
+                x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+                y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+            },
+            onClick: function(){} // Callback after click
+        }).showToast();
     } else {
+        // Si el producto no está en el carrito, agregarlo
+        Toastify({
+            text: "Producto agregado",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(to right, #4b33a8, #785ce9)",
+                borderRadius: "2rem",
+                textTransform: "uppercase",
+                fontSize: ".75rem"
+            },
+            offset: {
+                x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+                y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+            },
+            onClick: function(){} // Callback after click
+        }).showToast();
+
+        // Agregar el producto al carrito
         productoAgregado.cantidad = 1;
         productosEnCarrito.push(productoAgregado);
+        actualizarNumerito();
+        localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
     }
-
-    actualizarNumerito();
-
-    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
 }
 
 function actualizarNumerito() {
-    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    const nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
     numerito.innerText = nuevoNumerito;
 }
 
@@ -174,23 +172,16 @@ function buscarJuegos() {
     const input = document.getElementById("search-input");
     const term = input.value.toLowerCase();
     const juegosFiltrados = productos.filter(juego => juego.titulo.toLowerCase().includes(term));
-
     cargarProductos(juegosFiltrados);
 }
 
 function ordenarPorPopularidad() {
-    const juegosOrdenados = productos.sort((a, b) => {
-        // Ordenar por ID
-        return a.id.localeCompare(b.id);
-    });
+    const juegosOrdenados = [...productos].sort((a, b) => a.id.localeCompare(b.id));
     cargarProductos(juegosOrdenados);
 }
 
 function ordenarAlfabeticamente() {
-    const juegosOrdenados = productos.sort((a, b) => {
-        // Ordenar por título
-        return a.titulo.localeCompare(b.titulo);
-    });
+    const juegosOrdenados = [...productos].sort((a, b) => a.titulo.localeCompare(b.titulo));
     cargarProductos(juegosOrdenados);
 }
 
